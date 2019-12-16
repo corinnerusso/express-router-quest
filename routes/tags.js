264ec4c777aa3e047681ad8d0f48c66c37ff2fd7
Corinne Russo <corinne.russo@gmail.com>
2019-12-16 13:15:08 +0100

    quête express router avec bonus

--- routes/tags.js
+++ routes/tags.js
@@ -2,10 +2,17 @@ const express = require("express");
 
 const router = express.Router({ mergeParams: true });
 const fakeTags = require("../data/tags");
+const fakePosts = require("../data/posts");
 
 // Get a list of tags
 router.get("/", (req, res) => {
   res.json(fakeTags);
+});
+
+router.get("/:tagId/posts", (req, res) => {
+  const tagId = Number(req.params.tagId);
+  const postTags = fakePosts.filter(toto => toto.tag_ids.includes(tagId));
+  res.json(postTags);
 });
 
 module.exports = router;
